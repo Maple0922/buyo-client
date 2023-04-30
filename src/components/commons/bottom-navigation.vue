d07be301aa4d610e7c7049f445d2ba9fb072af80
Maple0922 <maple0922sept@gmail.com>
2023-05-01 02:51:02 +0900

    update: 下部の高さ調整

--- src/components/commons/bottom-navigation.vue
+++ src/components/commons/bottom-navigation.vue
@@ -1,52 +1,38 @@
 <template>
   <v-bottom-navigation
-    :elevation="4"
+    :elevation="0"
     grow
-    :bg-color="color"
+    color="indigo-darken-4"
     v-model="tabName"
     @update:modelValue="onTabChange"
     mandatory
-    :height="80"
+    height="72"
   >
     <v-btn value="list">
       <v-icon>mdi-calendar-range</v-icon>
-      一覧
+      <span class="mb-3">一覧</span>
     </v-btn>
     <v-btn value="reserve">
       <v-icon>mdi-calendar-edit</v-icon>
-      予約
+      <span class="mb-3">予約</span>
     </v-btn>
     <v-btn value="schedule">
       <v-icon>mdi-calendar-clock</v-icon>
-      日調
+      <span class="mb-3">日調</span>
     </v-btn>
   </v-bottom-navigation>
 </template>
 
 <script lang="ts" setup>
-import { computed, ref } from "vue";
+import { ref } from "vue";
 import { useRouter } from "vue-router";
 const router = useRouter();
 
-const tabName = ref<TabColor["name"]>("list");
+type TabName = "list" | "reserve" | "schedule";
 
-interface TabColor {
-  name: "list" | "reserve" | "schedule";
-  color: "blue-grey" | "teal" | "indigo";
-}
+const tabName = ref<TabName>("list");
 
-const tabColors: TabColor[] = [
-  { name: "list", color: "blue-grey" },
-  { name: "reserve", color: "teal" },
-  { name: "schedule", color: "indigo" },
-];
-
-const color = computed<TabColor["color"]>(
-  () =>
-    tabColors.find((tab) => tab.name === tabName.value)?.color ?? "blue-grey"
-);
-
-const onTabChange = (tabName: TabColor["name"]) => {
+const onTabChange = (tabName: TabName) => {
   router.push({ name: tabName });
 };
 </script>
