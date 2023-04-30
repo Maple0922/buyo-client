5948d1b377b1d62c932b498e5fac64d504e341dc
Maple0922 <maple0922sept@gmail.com>
2023-05-01 04:05:40 +0900

    update: パスエイリアスの設定

--- vite.config.ts
+++ vite.config.ts
@@ -1,35 +1,42 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import { resolve } from "path";
 
-import { VitePWA } from 'vite-plugin-pwa'
+import vue from "@vitejs/plugin-vue";
+
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vite-plugin
-import vuetify from 'vite-plugin-vuetify'
+import vuetify from "vite-plugin-vuetify";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      "@": resolve(__dirname, "./src"),
+    },
+  },
   plugins: [
-		vue(),
-		vuetify({ autoImport: true }),
-		VitePWA({
-			manifest: {
-				"lang": "ja",
-				"name": "BUYO v2",
-				"short_name": "BUYO",
-				"icons": [
-					{
-						"src": "/icons/android-chrome-192x192.png",
-						"sizes": "192x192",
-						"type": "image/png"
-					},
-				],
-				"start_url": "/",
-				"display": "standalone",
-				"background_color": "#ffffff",
-				"theme_color": "#ffffff",
-				"orientation": "portrait-primary",
-				"scope": "/",
-			},
-		}),
-	],
-})
+    vue(),
+    vuetify({ autoImport: true }),
+    VitePWA({
+      manifest: {
+        lang: "ja",
+        name: "BUYO v2",
+        short_name: "BUYO",
+        icons: [
+          {
+            src: "/icons/android-chrome-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+        ],
+        start_url: "/",
+        display: "standalone",
+        background_color: "#ffffff",
+        theme_color: "#ffffff",
+        orientation: "portrait-primary",
+        scope: "/",
+      },
+    }),
+  ],
+});
